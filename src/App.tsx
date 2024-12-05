b2785010ba8b8c0136e3e9a58b10e16d1b18f52a
denisgershovich <denis.gershovich@gmail.com>
2024-12-05 15:17:43 +0200

    chore: install and configure Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import { useState } from 'react';
 
 function App() {
 
-  return <div> demo </div>;
+  return     <h1 className="text-3xl font-bold underline text-orange-700">
+  Hello world!
+</h1>
+
 }
 
 export default App;
