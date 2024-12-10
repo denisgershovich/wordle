c51336ffa64fb0c4308584e403e1f7e87e4715e9
denisgershovich <denis.gershovich@gmail.com>
2024-12-10 14:13:58 +0200

    Create keyboard component

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,7 +1,26 @@
 
 const Keyboard = () => {
 
-    return <div>Keyboard</div>
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+    const handleClick = (letter: string) => {
+      console.log(`You clicked: ${letter}`);
+      // Add any logic you want when a letter is clicked
+    };
+  
+    return (
+      <div className=" border flex flex-wrap justify-center p-8 gap-2 ">
+        {letters.map((letter) => (
+          <button
+            key={letter}
+            onClick={() => handleClick(letter)}
+            className="bg-blue-500 text-white py-2 px-8 rounded hover:bg-blue-600 focus:outline-none"
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
+    );
 }
 
 export default Keyboard
