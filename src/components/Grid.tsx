d8d787b7c6ac79729bc2b3ed308de98fa32886bf
denisgershovich <denis.gershovich@gmail.com>
2024-12-10 14:13:37 +0200

    Create grid component

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,7 +1,14 @@
+import type { Cell } from "../interfaces"
 
-const Grid = () => {
+const Grid = ({grid}:{grid:Cell[][]}) => {
 
-    return <div>grid component</div>
+return <div className="flex flex-col gap-2">
+         {grid.map((row) => {
+             return <div className="flex gap-2">
+             {row.map((cell, i) => (<div className={`border-2 p-4 ${i === 1 ? 'border-red-500' : ''}`}></div>))}
+        </div>
+        })}
+    </div>
 }
 
 export default Grid
