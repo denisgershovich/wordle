9f54ea9c5a50773ef4723b7433c5e8b350b293e0
denisgershovich <denis.gershovich@gmail.com>
2024-12-10 14:14:36 +0200

    Add key down event listener

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Keyboard from './components/Keyboard';
 import Grid from './components/Grid';
+import type { Cell } from './interfaces';
+
+const ROW = 6
+const COL = 5
+
+const initialCellState: Cell[][] = Array.from({ length: ROW }, () =>
+  Array.from({ length: COL }, () => ({
+    value: null,
+    isMatch: false,
+    isInSolution: false
+  }))
+);
 
 function App() {
+  const [grid, setGrid] = useState(initialCellState)
 
-  return <main>
-    <Grid />
+  useEffect(() => {
+    const handleKeyDown = (event:KeyboardEvent) => {
+      const normalizedKey = event.key.toLowerCase()
+
+      if(normalizedKey.length === 1 && normalizedKey >= "a" && normalizedKey <= "z") {
+        console.log(`Key pressed: ${normalizedKey}`); 
+      }
+ 
+
+    }
+
+    document.addEventListener('keydown', handleKeyDown, true);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+
+  }, [])
+
+  return <main className='flex flex-col items-center gap-20 h-full py-16'>
+    <Grid grid={grid} />
     <Keyboard />
   </main>
 
